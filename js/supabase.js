f643eea6000b3d880253b95524e550fffd41fc25
personal-sities <umirbekovdide@gmail.com>
2026-03-16 20:17:51 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,36 +1,33 @@
 const SUPABASE_URL = "https://utwjvkooifuitpttrglw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_cOaIZv2uOu16ix7YJx3T_A_BXI-Vdb5";
 
-console.log("SUPABASE_URL =", SUPABASE_URL);
-
 const sb = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
 
-async function getCurrentUser() {
+async function getCurrentUser(){
   const { data, error } = await sb.auth.getUser();
-  if (error) throw error;
+  if(error) throw error;
   return data.user;
 }
 
-async function getMyProfile() {
+async function getMyProfile(){
   const user = await getCurrentUser();
-  if (!user) return null;
+  if(!user) return null;
 
   const { data, error } = await sb
-    .from("employees")
-    .select("*")
-    .eq("id", user.id)
-    .single();
+  .from("employees")
+  .select("*")
+  .eq("id", user.id)
+  .single();
 
-  if (error) throw error;
+  if(error) throw error;
 
   return data;
 }
 
-async function logoutUser() {
-  const { error } = await sb.auth.signOut();
-  if (error) throw error;
-  location.href = "login.html";
+async function logoutUser(){
+  await sb.auth.signOut();
+  location.href="login.html";
 }
