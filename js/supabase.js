ed9f3ccb7cdf933cab28f719690d06e4107ba6b3
personal-sities <umirbekovdide@gmail.com>
2026-03-16 18:33:23 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,45 +1,38 @@
-const SUPABASE_URL = "https://utwjvkoofiuitpttrglw.supabase.co"
+const SUPABASE_URL = "https://utwjvkooifuitpttrglw.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_cOaIZv2uOu16ix7YJx3T_A_BXI-Vdb5"
+const SUPABASE_ANON_KEY = "sb_publishable_cOaIZv2uOu16ix7YJx3T_A_BXI-Vdb5";
 
 const sb = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
-)
+);
 
 async function getCurrentUser() {
-
-  const { data, error } = await sb.auth.getUser()
-
-  if(error) throw error
-
-  return data.user
+  const { data, error } = await sb.auth.getUser();
+  if (error) throw error;
+  return data.user;
 }
 
-async function getMyProfile(){
+async function getMyProfile() {
+  const user = await getCurrentUser();
 
-  const user = await getCurrentUser()
-
-  if(!user) return null
+  if (!user) return null;
 
   const { data, error } = await sb
-  .from("employees")
-  .select("*")
-  .eq("id", user.id)
-  .single()
+    .from("employees")
+    .select("*")
+    .eq("id", user.id)
+    .single();
 
-  if(error) throw error
+  if (error) throw error;
 
-  return data
-
+  return data;
 }
 
-async function logoutUser(){
+async function logoutUser() {
+  const { error } = await sb.auth.signOut();
 
-  const { error } = await sb.auth.signOut()
+  if (error) throw error;
 
-  if(error) throw error
-
-  location.href="login.html"
-
+  location.href = "login.html";
 }
